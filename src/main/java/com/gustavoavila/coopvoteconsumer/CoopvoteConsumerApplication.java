e2f242d953d4d9578e9ebd5bfbde828c3ef3fde4
Gustavo Avila <gustavogavila01@gmail.com>
2022-04-09 22:06:32 -0300

    initial commit

---
+++ src/main/java/com/gustavoavila/coopvoteconsumer/CoopvoteConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoavila.coopvoteconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopvoteConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopvoteConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoavila/coopvoteconsumer/CoopvoteConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoavila.coopvoteconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopvoteConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
